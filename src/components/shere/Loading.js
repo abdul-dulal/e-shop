f66a352d606e93ce711e44a6e37fa8e5917f3313
abdul-dulal <dulal.dpi.387364@gmail.com>
2022-08-06 15:58:59 +0600

    contact page added

--- src/components/shere/Loading.js
+++ src/components/shere/Loading.js
@@ -1,9 +1,17 @@
 import React from "react";
-
+import { Puff } from "react-loader-spinner";
 const Loading = () => {
   return (
-    <div>
-      <p>Loading.....</p>
+    <div className="flex justify-center mt-5">
+      <Puff
+        height="80"
+        width="80"
+        radius="9"
+        color="green"
+        ariaLabel="three-dots-loading"
+        wrapperStyle
+        wrapperClass
+      />
     </div>
   );
 };
